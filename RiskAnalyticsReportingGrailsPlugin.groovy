3cf468a5feb9bda353b9a3a6e18ef718a572e7e2
Bjorn Zetterstrom <bjorn.zetterstrom@art-allianz.com>
2011-09-09 11:21:00 +0200

    new plugin version 0.2

--- RiskAnalyticsReportingGrailsPlugin.groovy
+++ RiskAnalyticsReportingGrailsPlugin.groovy
@@ -4,7 +4,7 @@ import org.pillarone.riskanalytics.reporting.gira.GIRAReportModel
 
 class RiskAnalyticsReportingGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
