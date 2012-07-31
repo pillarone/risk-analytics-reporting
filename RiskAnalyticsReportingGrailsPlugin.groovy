5fddf6d6a83dcc4eb949cb712e3d6e91e684009a
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2012-07-31 11:56:48 +0200

    upgrade to newest plugins

--- RiskAnalyticsReportingGrailsPlugin.groovy
+++ RiskAnalyticsReportingGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.pillarone.riskanalytics.reporting.gira.reports.UnderwritingReportMode
 
 class RiskAnalyticsReportingGrailsPlugin {
     // the plugin version
-    def version = "0.3.5"
+    def version = "0.3.6"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
