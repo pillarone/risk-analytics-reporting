8796750043f864f2677f24bc4593f11ddd8a9779
detlef-brendle <detlef.brendle@canoo.com>
2013-08-15 12:58:19 +0200

    revert change, due to wrong path

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,7 +10,7 @@ grails.project.dependency.resolution = {
         mavenRepo "https://repository.intuitive-collaboration.com/nexus/content/repositories/pillarone-public/"
     }
 
-    String ulcVersion = "ria-suite-2013-2"
+    String ulcVersion = "ria-suite-2012-u1-2"
 
     plugins {
         runtime ":background-thread:1.3"
@@ -24,15 +24,15 @@ grails.project.dependency.resolution = {
         runtime ":tomcat:2.2.1"
 
         compile "com.canoo:ulc:${ulcVersion}"
-        runtime "org.pillarone:pillar-one-ulc-extensions:1.0"
+        runtime "org.pillarone:pillar-one-ulc-extensions:0.5"
 
         test ":code-coverage:1.2.4"
         compile ":excel-import:1.0.0"
 
 
         if (appName == "risk-analytics-reporting") {
-            runtime "org.pillarone:risk-analytics-core:1.8-a4"
-            runtime ("org.pillarone:risk-analytics-application:1.8-a2") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.7"
+            runtime ("org.pillarone:risk-analytics-application:1.7") { transitive = false }
             runtime ("org.pillarone:risk-analytics-pc-cashflow:1.7") { transitive = false }
             runtime ("org.pillarone:risk-analytics-commons:1.7") { transitive = false }
         }
