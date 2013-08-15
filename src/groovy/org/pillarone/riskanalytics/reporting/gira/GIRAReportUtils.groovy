ec4038e5974be486d93aa928ae6fcf8de01506d1
detlef-brendle <detlef.brendle@canoo.com>
2013-08-15 12:09:13 +0200

    update versions; create new plugin version

--- src/groovy/org/pillarone/riskanalytics/reporting/gira/GIRAReportUtils.groovy
+++ src/groovy/org/pillarone/riskanalytics/reporting/gira/GIRAReportUtils.groovy
@@ -3,10 +3,10 @@ package org.pillarone.riskanalytics.reporting.gira
 import net.sf.jasperreports.engine.data.JRMapCollectionDataSource
 import net.sf.jasperreports.renderers.JCommonDrawableRenderer
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource
+import org.pillarone.riskanalytics.application.ui.base.model.modellingitem.ModellingInformationTableTreeModel
 import org.pillarone.riskanalytics.application.ui.util.DateFormatUtils
 import org.pillarone.riskanalytics.application.ui.simulation.view.impl.SimulationConfigurationView
 import org.pillarone.riskanalytics.application.ui.util.UIUtils
-import org.pillarone.riskanalytics.application.ui.base.model.ModellingInformationTableTreeModel
 import org.pillarone.riskanalytics.application.ui.result.view.ResultSettingsView
 import org.pillarone.riskanalytics.core.simulation.item.Simulation
 import org.pillarone.riskanalytics.core.simulation.item.Parameterization
